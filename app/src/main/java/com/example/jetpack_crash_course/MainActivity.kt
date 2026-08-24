2ed9bb855b6dc31ae7e2da18cbabb9cf837b5740
TonyBimenyi <tonyblaisb@gmail.com>
2026-08-25 00:27:54 +0200

    change

--- app/src/main/java/com/example/jetpack_crash_course/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_crash_course/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.jetpack_crash_course
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,20 +29,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-        HakunaMatata("Hoodlab")
+//        HakunaMatata("Hoodlab")
         }
     }
 }
 
 
 @Composable
-fun HakunaMatata(name: String){
+fun HakunaMatataContent()
+{
+    HakunaMatataImage()
+    HakunaMatataText("Hoodland")
+}
+
+@Composable
+fun HakunaMatataText(name: String){
     Text(
         text = "$name Hakuna Matata",
         fontWeight = FontWeight.Bold,
         color = Color.Green,
         fontSize = 20.sp,
         modifier = Modifier.padding(20.dp)
+
+    )
+}
+
+@Composable
+fun HakunaMatataImage() {
+    Image(
+        painter = painterResource(R.drawable.hakuna_matata),
+        contentDescription = "Hakuna Matata Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .clip(
+            RoundedCornerShape(30.dp)
+        )
+
     )
 }
 
@@ -45,8 +73,22 @@ fun HakunaMatata(name: String){
 @Composable
 
 fun PrevHakunaMatata(){
-    HakunaMatata("HoodLab")
+    HakunaMatataText("HoodLab")
 }
+
+@Preview(showBackground = true)
+@Composable
+fun PrevHakunaMatataImage(){
+    HakunaMatataImage()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevHakunaMatataContent(){
+    HakunaMatataContent()
+}
+
+
 //darel
 //        darel
 //        irashitsa dan nolwen
